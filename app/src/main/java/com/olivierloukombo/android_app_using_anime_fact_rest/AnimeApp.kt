785490bbbf3e31b0166d99bc5ca20c7016d40637
Olivier Loukombo <olivierloukombo@gmail.com>
2022-12-23 11:00:02 +0100

    add reusable components

---
+++ app/src/main/java/com/olivierloukombo/android_app_using_anime_fact_rest/AnimeApp.kt
@@ -0,0 +1,8 @@
+package com.olivierloukombo.android_app_using_anime_fact_rest
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AnimeApp: Application() {
+}
---
+++ app/src/main/java/com/olivierloukombo/android_app_using_anime_fact_rest/components/CircularProgress.kt
@@ -0,0 +1,20 @@
+package com.olivierloukombo.android_app_using_anime_fact_rest.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CircularProgress() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator()
+    }
+}
